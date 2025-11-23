682e40ecfb305caba4bf282a15f7f0613c0bd27a
sreyash <sreyash.s@outlook.com>
2025-11-24 05:22:32 +0530

    refactored html

--- tickets/urls.py
+++ tickets/urls.py
@@ -9,12 +9,13 @@ from .views import (
     DashboardTemplateView
 )
 
+app_name = 'tickets'
 urlpatterns = [
-    path('my/', TicketListView.as_view(), name='TicketListView'),
-    path('buy/', TicketPurchaseView.as_view(), name='TicketPurchaseView'),
-    path('dashboard/add-funds/', WalletBalanceUpdateView.as_view(), name='WalletBalanceUpdateView'),
-    path('scanner/scan/', TicketScanUpdateView.as_view(), name='TicketScanUpdateView'),
-    path('scanner/buy/', TicketPurchaseOfflineView.as_view(), name='TicketPurchaseOfflineView'),
-    path('scanner/', ScannerTemplateView.as_view(), name='ScannerTemplateView'),
-    path('dashboard/', DashboardTemplateView.as_view(), name='DashboardTemplateView')
+    path('my/', TicketListView.as_view(), name='ticket_list'),
+    path('buy/', TicketPurchaseView.as_view(), name='ticket_purchase'),
+    path('dashboard/add-funds/', WalletBalanceUpdateView.as_view(), name='wallet_balance_update'),
+    path('scanner/scan/', TicketScanUpdateView.as_view(), name='ticket_scan'),
+    path('scanner/buy/', TicketPurchaseOfflineView.as_view(), name='ticket_purchase_offline'),
+    path('scanner/', ScannerTemplateView.as_view(), name='scanner'),
+    path('dashboard/', DashboardTemplateView.as_view(), name='dashboard')
 ]
