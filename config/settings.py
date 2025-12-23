2d150be9af766ea6cb9199772572aebea1a42fca
sreyash <sreyash.s@outlook.com>
2025-12-24 01:32:43 +0530

    made email host configurable via env file

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.resend.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
